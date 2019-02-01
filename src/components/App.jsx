8ccf3c988ca18cf1b63c14aafb10d83f6bf36d73
Ilya Filippov <89099109104@mail.ru>
2019-02-01 17:40:13 +0300

    Init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import logo from '../logo.svg'
+import styles from './App.module.sass'
+
+class App extends Component {
+  render () {
+    return (
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className={styles.appLink}
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
